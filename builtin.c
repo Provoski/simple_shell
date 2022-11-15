ed29d7afa8c6ca98b3c8e9f49d33d632e9318443
Provoski <provo.iyen27@gmail.com>
2022-11-15 04:49:20 -0800

    file responsible for the execution of built in functions added

---
+++ builtin.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "simple_shell.h"
+
+/**
+ * my_exit - exit the shell program
+ * @str: array of strings
+ * Return: Nothing.
+ */
+int my_exit(char **str)
+{
+	if (strcmp(str[0], "exit") == 0)
+		exit(EXIT_SUCCESS);
+	return (0);
+}
+
+/**
+ * my_env - prints environmetal variable of the current enviroment
+ * @str: array of strings
+ * Return: 0 on success
+ */
+int my_env(char **str)
+{
+	(void)(str);
+
+	for (; *environ; environ++)
+	{
+		printf("%s\n", *environ);
+	}
+	return (0);
+}
